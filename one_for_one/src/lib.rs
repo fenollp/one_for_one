d4d57de2814d3a8a8dd2f4c493a9d62dc01261b1
Pierre Fenoll <pierrefenoll@gmail.com>
2024-03-26 17:30:11 +0100

    v2: seamless DX with task_local (#1)
    * WIP: seamless DX with task_local
    
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>
    
    * cleanup
    
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>
    
    * exploring
    
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>
    
    * add handle
    
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>
    
    * stack supervisors effortlessly
    
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>
    
    * fix supervized impl
    
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>
    
    * prepare for v2
    
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>
    
    * wrap up
    
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>
    
    ---------
    
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- one_for_one/src/lib.rs
+++ one_for_one/src/lib.rs
@@ -3,23 +3,61 @@ use std::future::Future;
 use tokio::{
     select,
     signal::unix::{signal, SignalKind},
-    task::JoinHandle,
+    task::{futures::TaskLocalFuture, JoinHandle},
 };
 use tokio_util::{sync::CancellationToken, task::TaskTracker};
+use tracing::warn;
 
 /// Listens to OS signals for ever. Completes on the first one received amon:
 /// * SIGINT (^C)
 /// * SIGTERM
 ///
 /// Usually, this is awaited on through `tokio::select!`
-/// to then call a given supervisor's `sup.die().await`.
+/// to then call a given supervisor's `Supervisor::terminate().await`.
+///
+/// Note: on Linux, a signal handler is registered for the whole process' life.
+/// Meaning that all later re-registers are ineffectual.
 pub async fn should_die() {
+    //TODO: take in bitflags
     let mut sigint = signal(SignalKind::interrupt()).expect("Failed to listen to SIGINT");
     let mut sigterm = signal(SignalKind::terminate()).expect("Failed to listen to SIGTERM");
     select! {
-        _ = sigint.recv() => tracing::warn!("Got ^C signal!"),
-        _ = sigterm.recv() => tracing::warn!("Got SIGTERM signal!"),
+        _ = sigint.recv() => warn!("Got ^C signal!"),
+        _ = sigterm.recv() => warn!("Got SIGTERM signal!"),
+        // TODO: also wait for Notify (all_waiters)
     }
+}
+
+tokio::task_local! {
+    static SUPERVIZED: Supervisor;
+}
+
+/// Gets a handle on this coode's Supervisor.
+///
+/// Returns `None` if outside of `supervized(async move { .. })`
+/// or of `sync_supervized(|| ..)`.
+#[inline]
+pub fn current() -> Option<Supervisor> {
+    SUPERVIZED.try_with(Clone::clone).ok()
+}
+
+/// Scopes (async) code that needs supervision.
+pub fn supervized<F>(f: F) -> TaskLocalFuture<Supervisor, F>
+where
+    F: Future,
+{
+    let slf = current().unwrap_or_default();
+    SUPERVIZED.scope(slf, f)
+}
+
+/// Scopes code that needs supervision.
+#[track_caller]
+pub fn sync_supervized<F, R>(f: F) -> R
+where
+    F: FnOnce() -> R,
+{
+    let slf = current().unwrap_or_default();
+    SUPERVIZED.sync_scope(slf, f)
 }
 
 /// Supervisor implements a supervision tree
@@ -29,6 +67,8 @@ pub async fn should_die() {
 /// This supervision strategy is was dubbed "one_for_one" by the Erlang people:
 /// See https://learnyousomeerlang.com/supervisors
 /// See https://www.erlang.org/doc/man/supervisor.html
+///
+/// Supervisor isn't Copy but it is Clone + Sized + Send + Sync
 #[derive(Clone, Debug, Default)]
 pub struct Supervisor {
     token: CancellationToken,
@@ -37,86 +77,72 @@ pub struct Supervisor {
 
 #[test]
 fn assert_all() {
-    fn assert_clone<T: Clone>() {}
-    fn assert_send<T: Send>() {}
-    fn assert_sized<T: Sized>() {}
-    fn assert_sync<T: Sync>() {}
-
-    assert_clone::<Supervisor>();
-    assert_send::<Supervisor>();
-    assert_sized::<Supervisor>();
-    assert_sync::<Supervisor>();
+    fn asserts<T: Clone + Sized + Send + Sync>() {}
+    asserts::<Supervisor>();
 }
 
-// // TODO: derive(spawn_under("sup")) to auto-impl Supervized
-
-// /// The revolution will be Supervized
-// pub trait Supervized {
-//     /// Consumes `sup` to keep in `self`
-//     fn set_supervisor(self, sup: Supervisor);
-// }
+const REASON: &str = "Not running within supervized(async move { .. }) or sync_supervized(|| ..)";
 
 impl Supervisor {
-    // /// Branch a `Supervized` instance under this tree
-    // pub fn supervize(&self, instance: impl Supervized) {
-    //     instance.set_supervisor(self.clone());
-    // }
-
     /// Spawn starts a supervised task, calling `spawn`.
+    ///
+    /// Panics: if not called within a supervized scope.
     #[inline]
     #[track_caller]
-    pub fn spawn<F, G>(&self, task: G) -> JoinHandle<F::Output>
+    pub fn spawn<F>(future: F) -> JoinHandle<F::Output>
     where
-        G: FnOnce(Self) -> F,
         F: Future + Send + 'static,
         F::Output: Send + 'static,
     {
-        let this = self.clone();
-        self.tasks.spawn(task(this))
+        let slf = current().expect(REASON);
+        slf.tasks.spawn(future)
     }
 
     /// Spawn starts a supervised task, calling `spawn_blocking`.
-    #[inline]
+    ///
+    /// Panics: if not called within a supervized scope.
     #[track_caller]
-    pub fn spawn_blocking<F, T>(&self, task: F) -> JoinHandle<T>
+    pub fn spawn_blocking<F, R>(f: F) -> JoinHandle<R>
     where
-        F: FnOnce(Self) -> T,
-        F: Send + 'static,
-        T: Send + 'static,
+        F: FnOnce() -> R + Send + 'static,
+        R: Send + 'static,
     {
-        let this = self.clone();
-        self.tasks.spawn_blocking(|| task(this))
+        let slf = current().expect(REASON);
+        slf.tasks.spawn_blocking(f)
     }
 
     /// Returns supervision state immediately
+    ///
+    /// Panics: if not called within a supervized scope.
     #[must_use]
-    pub fn is_cancelled(&self) -> bool {
-        self.token.is_cancelled()
+    pub fn is_cancelled() -> bool {
+        let slf = current().expect(REASON);
+        slf.token.is_cancelled()
     }
 
     /// A future that completes when the supervision
     /// tree is interrupted.
     /// Akin to Go's `<-ctx.Done()` chan recv.
-    #[allow(clippy::manual_async_fn)] // FIXME: try dropping (does it keep Send?)
-    pub fn done(&self) -> impl Future<Output = ()> + Send + '_ {
-        async move {
-            // Gets sibling tokens (don't die if a sibling dies)
-            let _: () = self.token.child_token().cancelled().await;
-            tracing::warn!("Children cancelled");
-        }
+    ///
+    /// Panics: if not called within a supervized scope.
+    pub async fn done() {
+        let slf = current().expect(REASON);
+        // Gets sibling tokens (don't die if a sibling dies)
+        let _: () = slf.token.child_token().cancelled().await;
+        warn!("Children cancelled");
     }
 
     /// A future that triggers the interruption or termination
     /// of the supervision tree.
     /// Completes when all children are confirmed dead.
-    #[allow(clippy::manual_async_fn)] // FIXME: try dropping (does it keep Send?)
-    pub fn die(&self) -> impl Future<Output = ()> + Send + '_ {
-        async move {
-            tracing::warn!("Terminating...");
-            self.tasks.close();
-            self.token.cancel();
-            self.tasks.wait().await;
-            tracing::warn!("Terminated!");
-        }
+    ///
+    /// Panics: if not called within a supervized scope.
+    pub async fn terminate() {
+        let slf = current().expect(REASON);
+        warn!("Terminating...");
+        slf.tasks.close();
+        slf.token.cancel();
+        slf.tasks.wait().await;
+        warn!("Terminated!");
     }
 }
